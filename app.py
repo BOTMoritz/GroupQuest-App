a087def4ef22bd6792bc50e09e0b829283ff13bb
konign44-a11y <konign44@gmail.com>
2026-05-06 10:26:15 +0200

    Login Page init create

--- app.py
+++ app.py
@@ -1,5 +1,29 @@
 import streamlit as st
 
-st.title("Hello, Streamlit!")
-st.write("This is a simple Streamlit app.")
 
+
+def login_seite():
+    st.title("Willkommen in der Groupquest-App!")
+
+    username = st.text_input("Benutzername")
+    passwort = st.text_input("Passwort", type="password")
+
+    if st.button("Login"):
+        if check_login(username, passwort):
+            st.success("Erfolgreich eingeloggt!")
+        else:
+            st.error("Ungültige Anmeldedaten. Bitte versuchen Sie es erneut.")
+
+def check_login(username, passwort):
+    return username == "admin" and passwort == "password"
+
+
+
+
+def logout():
+    if st.button("Logout"):
+        st.success("Erfolgreich ausgeloggt!")
+
+if __name__ == "__main__":
+    login_seite()
+    logout()
